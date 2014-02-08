8dd1515cb5157eda73b98fc4f69f97cd3cd03a0a
ikaruga <ikaruga777@gmail.com>
2014-02-08 23:49:06 +0900

    引数指定できる

--- match.rb
+++ match.rb
@@ -3,12 +3,27 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-uri = URI.parse('http://sdvx-s.coresv.com/user/ikaruga.json')
-json = Net::HTTP.get(uri)
-parsed = JSON.parse(json)
-parsed["profile"]["tracks"].each do|track|
-	puts track["title"]
+
+#users =  %w{ikaruga FM}
+
+def getjson(user)
+	uri = URI.parse('http://sdvx-s.coresv.com/user/'+ user +'.json')
+	getjson = Net::HTTP.get(uri)
 end
 
+def main
+	users = ARGV
+	if users.empty?
+		puts "input user names"
+		return
+	end
+	
+	parsed = JSON.parse(getjson(users[0]))
+	parsed["profile"]["tracks"].each do|track|
+		puts track["title"]
+		puts track["exhaust"]["highscore"]
+	end
+end
 
+main()
 	
